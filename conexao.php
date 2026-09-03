bea59ed2787b40901d989db28880b7f412f64e86
Marco Túlio <marco.souza300322@gmail.com>
2026-09-02 21:56:08 -0300

    Tela de login finalizada

--- conexao.php
+++ conexao.php
@@ -1,15 +1,15 @@
 <?php
 
-$host= "localhost";
-$usuario="root";
-$senha="";
-$banco="hotel_db";
+$host = "localhost";
+$usuario = "root";
+$senha = "";
+$banco = "hotel_db";
 
-$conexao=mysqli_connect($host,$usuario,$senha,$banco);
+$conexao = mysqli_connect($host, $usuario, $senha, $banco);
 
 if($conexao){
-   echo "conectou ao banco de dados";
-} else{
-    echo "nao conectou ao banco de dados";
+    echo "Conectou ao banco de dados<br>";
+}else{
+    echo "Não conectou ao banco de dados<br>";
 }
 ?>
