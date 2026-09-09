6f756cdbee2db5ce7578a341ffa8974924893811
Bianca <0001229474@senaimgaluno.com.br>
2026-09-08 21:16:40 -0300

    código do arquivo autenticar.php

---
+++ autenticar.php
@@ -0,0 +1,23 @@
+ <?php
+ include_start();
+ 
+$email = $_POST['email'];
+$senha = $_POST['senha'];
+
+$sql = "SELECT * FROM clientes WHERE email = '$email';
+$resultado = mysqli_query
+($conexao,$sql);
+
+if(mysqli_num_rows($resultado) > 0){
+$cliente = mysqli_fetch_assoc($resultado);
+
+if($senha == $cliente["senha"]){
+$resultado = ["email"] = $email;
+header("localion:minhas_recervas.php");}
+
+else{
+    acho"senha errada!";
+    acho"<br><a hret='login.html'>Voltar</a>";
+}
+else{
+    acho"E-mail nao encontrado!"
