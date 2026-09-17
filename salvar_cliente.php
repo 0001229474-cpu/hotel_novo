9786472daaecaad4ffab69a20d60263cc98cb493
Bianca <0001229474@senaimgaluno.com.br>
2026-09-16 22:05:00 -0300

    salva

--- salvar_cliente.php
+++ salvar_cliente.php
@@ -17,21 +17,6 @@ if(mysqli_query($conexao,$sql)){
     echo "<br>Error 404";
 }
 
-require_once "conexao.php";
-
-$nome = $_POST['nome'];
-$email = $_POST['email'];
-$telefone = $_POST['telefone'];
-$senha = $_POST['senha'];
-
-$sql = "INSERT INTO clientes (nome, email, telefone, senha)
-        VALUES ('$nome', '$email', '$telefone', '$senha')";
-
-if (mysqli_query($conexao, $sql)) {
-   
-} else {
-    
-}
 
 ?>
 
